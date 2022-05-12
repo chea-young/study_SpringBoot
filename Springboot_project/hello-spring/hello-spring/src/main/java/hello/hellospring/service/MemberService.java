4e3177c99079a7ee7ce3dc5513bb59af61498a61
chea-young <63410509+chea-young@users.noreply.github.com>
2022-05-13 00:52:30 +0900

    feat: add APO 스프링 입문

--- Springboot_project/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Springboot_project/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -27,9 +27,16 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms"); // 걸리는 시간 조회
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -43,9 +50,13 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 }
